f2e425c6afcb29cc09e4c6d705608d654a343958
Artur Dwornik <artur.dwornik@wikia-inc.com>
2013-09-18 23:45:23 +0200

    reimplement AggregatorActor using akka FSM

--- scala-indexer/src/main/scala/com/wikia/search/demo/util/AggregatorActor.scala
+++ scala-indexer/src/main/scala/com/wikia/search/demo/util/AggregatorActor.scala
@@ -1,39 +1,44 @@
 package com.wikia.search.demo.util
 
-import collection.mutable.ListBuffer
-import akka.actor.{Actor, ActorRef}
+import akka.actor.{LoggingFSM, Actor, ActorRef}
+import collection.immutable
+import scala.concurrent.duration._
+
+object AggregatorActor {
+  trait State
+  case object Waiting extends State
+  case object Hoarding extends State
+
+  case class Data[T]( queue: immutable.List[T] )
+}
 
 /**
  * Aggregates messages into fixed size chunks and sends chunks to consumer.
- * @param aggregateSize
- * @param consumer
- * @tparam T
+ * @param maxAggregateSize max size of batch of data sent to consumer
+ * @param consumer consumer of output batches
+ * @tparam T type of received messages
  */
-class AggregatorActor[T]( val aggregateSize: Int, val consumer: ActorRef ) extends Actor {
-  var currentList:ListBuffer[T] = ListBuffer[T]()
+class AggregatorActor[T]( val maxAggregateSize: Int, val consumer: ActorRef )
+  extends Actor with LoggingFSM[AggregatorActor.State, AggregatorActor.Data[T]] {
+  import AggregatorActor._
+  startWith(Waiting, Data(immutable.List()))
 
-  def receive = {
-    case None => finish()
-    case element:T => addAndCheck(element)
-    case _ => println("unknown element in aggregator")
+  when(Waiting) {
+    case Event(message:T, _) => goto(Hoarding) using Data( List(message) )
   }
 
-  def closeAndProduce() {
-    val list:ListBuffer[T] = currentList
-    currentList = ListBuffer[T]()
-    println("sending:" + list.length)
-    consumer ! list.result()
-  }
-
-  def addAndCheck( element:T ) {
-    currentList.append(element)
-    if ( currentList.length >= aggregateSize ) {
-      closeAndProduce()
+  when(Hoarding, stateTimeout = 1.second) {
+    case Event(StateTimeout, Data(queue)) => goto(Waiting) using Data(List())
+    case Event(message:T, Data(queue)) => {
+      if (queue.length == maxAggregateSize) goto(Waiting) using Data(List())
+      else goto(Hoarding) using Data(message::queue)
     }
   }
 
-  def finish() = {
-    closeAndProduce()
-    consumer ! None
+  onTransition {
+    case Hoarding -> Waiting =>
+      stateData match {
+        case Data(queue) => consumer ! queue
+      }
   }
 }
