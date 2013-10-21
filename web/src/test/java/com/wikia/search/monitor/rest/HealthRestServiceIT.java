2c49db7de302e2cd890ead9d559cf05eb2d57a48
Artur Dwornik <artur.dwornik@wikia-inc.com>
2013-10-22 00:52:52 +0200

    prepare for solr integration tests

--- web/src/test/java/com/wikia/search/monitor/rest/HealthRestServiceIT.java
+++ web/src/test/java/com/wikia/search/monitor/rest/HealthRestServiceIT.java
@@ -1,6 +1,13 @@
 package com.wikia.search.monitor.rest;
 
+import com.wikia.search.testing.TransientSolrFactory;
 import org.apache.cxf.jaxrs.client.WebClient;
+import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.embedded.EmbeddedSolrServer;
+import org.apache.solr.core.CoreContainer;
+import org.apache.solr.core.SolrConfig;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -8,14 +15,17 @@ import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.core.Response;
 
+import java.io.IOException;
+
 import static org.junit.Assert.assertEquals;
 
 public class HealthRestServiceIT {
     private static Logger logger = LoggerFactory.getLogger(HealthRestServiceIT.class);
     private static String endpointUrl;
+    private SolrServer solrServer;
 
-    @BeforeClass
-    public static void beforeClass() {
+    @Before
+    public void beforeClass() throws IOException, SolrServerException {
         endpointUrl = System.getProperty("service.url") + "api/health";
         logger.info(endpointUrl);
     }
